a094b83a99e01eee9c5c93e986522c9e11f17001
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-04-16 22:10:27 +0530

    done

---
+++ js/qrgenerator.js
@@ -0,0 +1,25 @@
+
+const generator = document.querySelector(".generator"),
+qrInput = generator.querySelector(".form input"),
+generateBtn = generator.querySelector(".form button"),
+qrImg = generator.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        generator.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        generator.classList.remove("active");
+        preValue = "";
+    }
+});
